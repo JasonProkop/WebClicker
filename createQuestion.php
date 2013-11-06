b07b3ebca1b67348f73f909a7ae13e156f2a81d2
zillionk <zillionk1973@gmail.com>
2013-11-05 23:39:50 -0600

    modify the looks of createQuestion page
    make it more easy to use.

--- createQuestion.php
+++ createQuestion.php
@@ -1,14 +1,34 @@
 <!doctype html>
 <html>
 <head>
-	<title>jQuery Mobile Page</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<link rel="stylesheet" href="themes/webclicker-usask.min.css" />
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" />
-	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<title>
+			WebClicker
+		</title>
+		<link rel="stylesheet" href="themes/webclicker-usask.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile.structure-1.3.2.min.css" />
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+
+		<style type='text/css'>
+		/* Style sheet to restrict the page on large browsers */
+
+		html { background-color: #333; }
+		/* this line detects browsers which have a minimum width of 600
+		 * pixels and only applies the changes to them */
+		@media only screen and (min-width: 600px){
+		    .ui-page {
+			width: 600px !important;
+			margin: 0 auto !important;
+			position: relative !important;
+			border-right: 5px #666 outset !important;
+			border-left: 5px #666 outset !important;
+		    }
+        }
+</style>
+
 	<script>
 	$(document).on('pageinit', '#q1', function () {
 		    var current = 2;
@@ -115,13 +135,15 @@
 		    });
 		});
 	</script>
-	
+
 </head>
 <body>
 
- <div data-role="page" id="q1">
-     <div data-role="header" data-theme="b">
-         <h1>Poll X2F4</h1>
+ <div data-role="page" id="q1" data-add-back-btn="true">
+     <div data-role="header" data-theme="c" >
+        <h1>Poll X2F4</h1>
+       <a href="index.php"  data-role="button" class="ui-btn-left" data-inline="true" data-icon="home">Home</a>
+
      </div>
      <div data-role="content">
          <form>
@@ -146,18 +168,18 @@
                 <fieldset class="ui-grid-a">
                 	<div class="ui-block-a"><input type="button" data-mini="true" id="add" value="Add response" />   </div> 
                 	<div class="ui-block-b"><input type="button" data-mini="true" id="remove" value="Remove response" /></div>
-
 				</fieldset>
- 
              </div>
          </form>
      </div>
-      <div data-role="footer" data-theme="b" data-position="fixed">
-        <fieldset class="ui-grid-b">
-                	<div class="ui-block-a"><input type="button"  id="add" value="Return" data-icon="arrow-l" data-iconpos="left"/>   </div> 
-                 	<div class="ui-block-b"><input type="button" data-theme="e" id="remove" value="All Done!" data-icon="check" data-iconpos="left"/></div>
-					<div class="ui-block-c"><input type="button" id="remove" value="Add Question" /></div>
-		</fieldset>
+      <div data-role="footer" data-theme="c" data-position="fixed">
+			<h1> </h1>
+    	<input type="submit" data-theme="b" id="submit" value="All Done!" data-icon="check" class="ui-btn-left" data-iconpos="left"/>
+    	 <div data-role="controlgroup" data-type="horizontal" class="ui-btn-right">
+			<input type="button"  id="prevq"  data-icon="arrow-l" data-iconpos="notext" value="Prev Question" />
+			<input type="submit"  id="submit" value="Question List" data-icon="bars" data-iconpos="left"/>
+			<input type="button" id="nextq"   data-icon="arrow-r" data-iconpos="notext" value="Next Question" />
+		</div>
      </div>
  </div>
 
