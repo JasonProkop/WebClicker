60ca21c2a8fc16fb49dfba1dcd8c0a7225c15c82
BradySmith <brady_smith@outlook.com>
2013-11-23 02:16:18 -0600

    added some welcome text

--- index.php
+++ index.php
@@ -16,6 +16,10 @@
 	<body>
 		<div id="homepage" data-role="page" data-title="WebClicker - homepage" data-theme='a'>
 			<?php drawHeader(); ?>
+			<div data-role"content" div id="center_box" div style="margin:0 auto;margin-left:auto;margin-right:auto;align:center;text-align:center;width:75%">
+				<h2>Welcome to Web Clicker!</h2>
+				<p>Use the buttons at the bottom of the page to get started or check out one of these recently created polls.</p>
+			</div>
 			<div data-role="collapsible" data-collapsed="false">
 			  <h3>Search Poll by name:</h3>
 				<ul data-role="listview" data-filter="true" data-inset="true" data-filter-reveal="true" data-filter-placeholder="Search Poll by Name...">
